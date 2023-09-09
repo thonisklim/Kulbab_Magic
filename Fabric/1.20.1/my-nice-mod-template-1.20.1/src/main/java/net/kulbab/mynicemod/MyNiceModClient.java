6ece440152318d4dfdc32a4d3dfdb9633c85b870
Kulbab <thonisklim@gmail.com>
2023-09-10 00:44:45 +0300

    started my education in mc modding!

---
+++ Fabric/1.20.1/my-nice-mod-template-1.20.1/src/main/java/net/kulbab/mynicemod/MyNiceModClient.java
@@ -0,0 +1,10 @@
+package net.kulbab.mynicemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyNiceModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
